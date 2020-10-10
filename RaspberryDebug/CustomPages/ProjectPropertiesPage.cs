d0be45ec9af1ff23def7a5226616fa6a48eca8bf
Jeff Lill <jeff@lilltek.com>
2020-10-09 21:07:31 -0700

    Project properties in progress

---
+++ RaspberryDebug/CustomPages/ProjectPropertiesPage.cs
@@ -0,0 +1,57 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    ProjectPropertiesPage.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:   Open Source
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+using Microsoft.VisualStudio.Editors.PropertyPages;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Settings;
+
+namespace RaspberryDebug
+{
+    /// <summary>
+    /// Implements the custom Raspberry project debug properties page.
+    /// </summary>
+    [ComVisible(true)]
+    [Guid("8b37874b-9f3f-4634-9f43-427d1d08ff5f")]
+    [ProvideObject(typeof(ProjectPropertiesPage))]
+    public class ProjectPropertiesPage : PropPageBase
+    {
+        /// <inheritdoc/>/>
+        protected override Type ControlType
+        {
+            get { return typeof(ProjectPropertiesPanel); }
+        }
+
+        /// <inheritdoc/>/>
+        protected override string Title
+        {
+            get { return "Debug Raspberry"; }
+        }
+
+        /// <inheritdoc/>/>
+        protected override Control CreateControl()
+        {
+            return new ProjectPropertiesPanel();
+        }
+    }
+}
---
+++ RaspberryDebug/CustomPages/ProjectPropertiesPanel.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace RaspberryDebug
+{
+    partial class ProjectPropertiesPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(69, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hello World!";
+            // 
+            // ProjectPropertiesPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Name = "ProjectPropertiesPanel";
+            this.Size = new System.Drawing.Size(375, 222);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
