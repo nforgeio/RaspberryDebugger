92851fd83decb09dac74f19a8f3a0b4963beb445
Jeff Lill <jeff@lilltek.com>
2020-12-21 11:55:06 -0800

    Debug Setting dialog: right-align labels

--- RaspberryDebugger/Dialogs/SettingsDialog.Designer.cs
+++ RaspberryDebugger/Dialogs/SettingsDialog.Designer.cs
@@ -89,7 +89,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 49);
+            this.label1.Location = new System.Drawing.Point(30, 49);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(73, 13);
             this.label1.TabIndex = 3;
