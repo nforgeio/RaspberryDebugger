2296fc6335384a0894597ecc4331912bd0659547
StanDeMan <StanislausE@gmx.de>
2022-05-03 02:58:56 +0200

    Create EnumMemberAttributeExtension

---
+++ RaspberryDebugger/Extensions/Extension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace RaspberryDebugger.Extensions
+{
+    public static class EnumMemberAttributeExtension
+    {
+        /// <summary>
+        /// Gets the type of the attribute of.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumVal">The enum value.</param>
+        /// <returns>value</returns>
+        public static T GetAttributeOfType<T>(this Enum enumVal) where T : Attribute
+        {
+            var memInfo = enumVal.GetType().GetMember(enumVal.ToString());
+            var attributes = memInfo[0].GetCustomAttributes(typeof(T), false);
+
+            return (attributes.Length > 0) 
+                ? (T)attributes[0] 
+                : null;
+        }
+    }
+}
