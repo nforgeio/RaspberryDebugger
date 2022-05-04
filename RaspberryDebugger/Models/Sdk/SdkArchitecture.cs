6a67640e89961a19cffc9c708941d7c11f573fe4
StanDeMan <StanislausE@gmx.de>
2022-05-04 22:19:23 +0200

    Code cleansing: add comment and add to dictionary

--- RaspberryDebugger/Models/Sdk/SdkArchitecture.cs
+++ RaspberryDebugger/Models/Sdk/SdkArchitecture.cs
@@ -14,7 +14,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 using System.Runtime.Serialization;
 
 namespace RaspberryDebugger.Models.Sdk
@@ -37,6 +36,9 @@ namespace RaspberryDebugger.Models.Sdk
         ARM64
     }
 
+    /// <summary>
+    /// Enumerates the supported architecture bitness.
+    /// </summary>
     internal enum Platform
     {
         /// <summary>
