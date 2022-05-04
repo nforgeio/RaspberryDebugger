6b0ca59e4530a8c7bc0a1eb65a2305c3863cb84b
StanDeMan <StanislausE@gmx.de>
2022-05-04 21:31:07 +0200

    Remove misplaced comment

--- RaspberryDebugger/Commands/DebugStartCommand.cs
+++ RaspberryDebugger/Commands/DebugStartCommand.cs
@@ -54,7 +54,6 @@ namespace RaspberryDebugger.Commands
 
         /// Initializes a new instance of the <see cref="DebugStartCommand"/> class.
         /// Adds our command handlers for menu (commands must exist in the command table file)
-        /// </summary>
         /// <param name="package">Owner package, not null.</param>
         /// <param name="commandService">Command service to add command to, not null.</param>
         private DebugStartCommand(AsyncPackage package, OleMenuCommandService commandService)
