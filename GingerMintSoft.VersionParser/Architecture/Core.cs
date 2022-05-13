7c4a5e5fae63ba36158afd481565ee2f264b9847
StanDeMan <StanislausE@gmx.de>
2022-05-14 01:50:33 +0200

    Reorder enums

--- GingerMintSoft.VersionParser/Architecture/Core.cs
+++ GingerMintSoft.VersionParser/Architecture/Core.cs
@@ -7,21 +7,25 @@ namespace GingerMintSoft.VersionParser.Architecture
         /// <summary>
         /// 3.1 version
         /// </summary>
-        [EnumMember(Value = "3.1")] Core3,
+        [EnumMember(Value = "3.1")] 
+        Core3,
 
         /// <summary>
         /// 5.0 version
         /// </summary>
-        [EnumMember(Value = "5.0")] Core5,
+        [EnumMember(Value = "5.0")] 
+        Core5,
 
         /// <summary>
         /// 6.0 version
         /// </summary>
-        [EnumMember(Value = "6.0")] Core6,
+        [EnumMember(Value = "6.0")] 
+        Core6,
 
         /// <summary>
         /// 7.0 version
         /// </summary>
-        [EnumMember(Value = "7.0")] Core7
+        [EnumMember(Value = "7.0")] 
+        Core7
     }
 }
