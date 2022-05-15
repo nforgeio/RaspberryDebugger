f71c0005b13fdd370f6388a6fce1e01e05323530
StanDeMan <StanislausE@gmx.de>
2022-05-15 20:01:39 +0200

    Add related test for simple internet check

---
+++ GingerMintSoft.VersionParser.Test/CheckInternetConnection.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using GingerMintSoft.VersionParser.Connection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GingerMintSoft.VersionParser.Test
+{
+    [TestClass]
+    public class CheckInternetConnection
+    {
+        [TestMethod]
+        public async Task CheckTest()
+        {
+            var ok = await Internet.CheckAsync();
+            Assert.IsTrue(ok);
+        }
+    }
+}
