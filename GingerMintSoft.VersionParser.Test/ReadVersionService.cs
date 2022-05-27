0e4e547b1188b804dfa7ada8b3c03741b3f8e814
StanDeMan <StanislausE@gmx.de>
2022-05-27 11:01:41 +0200

    Implement VersionFeedService test

---
+++ GingerMintSoft.VersionParser.Test/ReadVersionService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GingerMintSoft.VersionParser.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GingerMintSoft.VersionParser.Test
+{
+    [TestClass]
+    public class ReadVersionService
+    {
+        [TestMethod]
+        public async Task ReadVersionServiceAsync()
+        {
+            const string uri = @"https://dotnetverionfeed.azurewebsites.net/version";
+            var ret = await new Request().ReadVersionFeedService(uri);
+
+            Assert.IsNotNull(ret);
+            Debug.Print(ret);
+        }
+    }
+}
