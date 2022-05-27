001f37623f3c1e444a8291282f393ae1c8942e1a
StanDeMan <StanislausE@gmx.de>
2022-05-27 11:01:14 +0200

    Implement ReadVersionFeedService

---
+++ GingerMintSoft.VersionParser/Web/Request.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GingerMintSoft.VersionParser.Web
+{
+    public class Request
+    {
+        public async Task<string> ReadVersionFeedService(string uri)
+        {
+            string responseBody;
+
+            try
+            {
+                var response = await new HttpClient().GetAsync(uri);
+                response.EnsureSuccessStatusCode();
+
+                responseBody = await response.Content.ReadAsStringAsync();
+            }
+            catch (Exception)
+            {
+                responseBody = string.Empty;
+            }
+
+            return responseBody;
+        }
+    }
+}
