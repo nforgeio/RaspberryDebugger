bf53d2602247bd3a92c9b0118ddf41ca8cdc37e9
StanDeMan <StanislausE@gmx.de>
2022-06-05 11:20:53 +0200

    Small upgrades to get code optimized

--- Test/Blinkie/WebApp/Startup.cs
+++ Test/Blinkie/WebApp/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -17,7 +13,7 @@ namespace WebApp
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
