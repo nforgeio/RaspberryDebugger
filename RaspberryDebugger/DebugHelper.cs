6a610afc1ccdfbaedde49d735f59d443ea0baa2a
Jeff Lill <jeff@lilltek.com>
2023-10-24 11:20:41 -0700

    Revert "Allow Raspberry Pi 2 Zero"
    This reverts commit 8219ff5625d024dcb26f8f2bce46ef36ab64d76b.

--- RaspberryDebugger/DebugHelper.cs
+++ RaspberryDebugger/DebugHelper.cs
@@ -542,13 +542,12 @@ windir
             // .NET Core only supports Raspberry models 3 and 4.
 
             if (!connection.PiStatus.RaspberryModel.StartsWith("Raspberry Pi 3 Model") &&
-                !connection.PiStatus.RaspberryModel.StartsWith("Raspberry Pi 4 Model") &&
-                !connection.PiStatus.RaspberryModel.StartsWith("Raspberry Pi Zero 2"))
+                !connection.PiStatus.RaspberryModel.StartsWith("Raspberry Pi 4 Model"))
             {
                 await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
 
                 MessageBoxEx.Show(
-                    $"Your [{connection.PiStatus.RaspberryModel}] is not supported.  .NET Core requires a Raspberry Model 3, 4 or Pi Zero 2.",
+                    $"Your [{connection.PiStatus.RaspberryModel}] is not supported.  .NET Core requires a Raspberry Model 3 or 4.",
                     $"Raspberry Not Supported",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Error);
